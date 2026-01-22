8a037ec79f18e2026e1f5cd2e45085c2654f4663
Nav-2003 <88257725+Nav-2003@users.noreply.github.com>
2026-01-22 21:26:27 +0530

    Enhance socket connection settings
    Update socket connection configuration with path and transports.

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
-const Api=import.meta.env.VITE_BACKEND_API;
 
-const socket = io(`${Api}`);
+const Api = import.meta.env.VITE_BACKEND_API;
+
+const socket = io(Api, {
+  path: "/socket.io",
+  transports: ["websocket"]  
+});
 
 export default socket;
